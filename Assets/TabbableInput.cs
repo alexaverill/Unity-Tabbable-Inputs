dd193b2728d5565337353abb900cdaced2dc102f
Alex <alex.averill2013@gmail.com>
2018-11-26 20:03:50 -0700

    Initial commit to add code

---
+++ Assets/TabbableInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TabbableInput : MonoBehaviour {
+
+	public List<string> test = new List<string>();
+	public List<GameObject> Inputs = new List<GameObject>();
+	public int position = 0;
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyDown(KeyCode.Tab)){	
+		
+			if(Inputs[position].GetComponent<TMP_InputField>() != null){
+				var input = Inputs[position].GetComponent<TMP_InputField>();
+				input.Select();
+				//input.ActivateInputField();
+			}else if(Inputs[position].GetComponent<InputField>() != null){
+				var input = Inputs[position].GetComponent<InputField>();
+				
+				input.Select();
+			}else if(Inputs[position].GetComponent<Button>() != null){
+				var input = Inputs[position].GetComponent<Button>();
+				input.Select();
+			}else if(Inputs[position].GetComponent<TMP_Dropdown>() != null){
+				var input = Inputs[position].GetComponent<TMP_Dropdown>();
+				input.Select();
+				input.ActivateInputField();
+			}
+			
+			position ++;
+		}
+	}
+}
